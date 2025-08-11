67cfdbb8561a8a3c4080f4dd2e31e8a213c2303e
Jun Sasaki <jun.c.sasaki@gmail.com>
2025-08-11 13:47:58 +0900

    test commit

--- .githooks/pre-commit.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-REM Wrapper for Windows to run the PowerShell-based pre-commit hook.
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0pre-commit.ps1"
-set EXITCODE=%ERRORLEVEL%
-endlocal & exit /b %EXITCODE%
