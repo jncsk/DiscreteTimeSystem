8a2bbdb4d3cd947ca33b50651a662f2a584a7f15
Jun Sasaki <jun.c.sasaki@gmail.com>
2025-08-12 14:06:22 +0900

    refactor: modify some codes - Change g_matrix_last_error.code to g_last_error.code to follow   current implementation - Change type of matrix_exp_exponetial, void to CoreErrorStatus

--- DiscreteTimeSystemLib/source/linalg/matrix_exp.c
+++ DiscreteTimeSystemLib/source/linalg/matrix_exp.c
@@ -1,12 +1,14 @@
 ﻿#pragma once
 
 #include <stdio.h>
+#include <math.h>
+#include "core_error.h"
 #include "matrix_core.h"
 #include "matrix_exp.h"
 #include "matrix_ops.h"
-#include <math.h>
+#include "core_math_utils.h"
 
-void matrix_exp_exponential(const Matrix* A, double t, Matrix* result)
+CoreErrorStatus matrix_exp_exponential(const Matrix* A, double t, Matrix* result)
 {
 	if (A == NULL || result == NULL) {
 		CORE_ERROR_SET(CORE_ERROR_NULL);
@@ -18,33 +20,10 @@ void matrix_exp_exponential(const Matrix* A, double t, Matrix* result)
 	}
 
 	int k = 0;
+	double pow;
+	CoreErrorStatus status = CORE_ERROR_SUCCESS;
 
-	int status = 0;
-	Matrix* At = matrix_core_create(3, 3, &status);
-	status = matrix_ops_set_zero(At);
-	if (status != CORE_ERROR_SUCCESS)
-	{
-		CORE_ERROR_SET(status);
-		return status;
-	}
-
-	for (int i = 0; i < A->rows; i++)
-		for (int j = 0; j < A->cols; j++) {
-			{
-				printf("%d\n", i);
-				matrix_ops_set(At, i, j, matrix_ops_get(A, i, j, &status) * t);
-				if (status != CORE_ERROR_SUCCESS) {
-					CORE_ERROR_SET(status);
-					return status;
-				}
-			}
-
-			matrix_ops_print(A);
-			matrix_ops_print(At);
-
-		}
-	return;
-
+	return CORE_ERROR_SUCCESS;
 }
 
 
--- DiscreteTimeSystemLib/source/main.c
+++ DiscreteTimeSystemLib/source/main.c
@@ -7,6 +7,108 @@
 
 int main()
 {	
+	CoreErrorStatus status = CORE_ERROR_SUCCESS;
+	StateSpaceModel* matrix = state_space_create(3, 1, 1, &status);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	printf("=======matrix->A=========\n");
+	status = matrix_ops_set(matrix->A, 0, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 0, 1, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 0, 2, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 1, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 1, 1, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 1, 2, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 2, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 2, 1, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->A, 2, 2, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	status = matrix_ops_print(matrix->A);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	printf("=======matrix->B=========\n");
+	status = matrix_ops_set(matrix->B, 0, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->B, 1, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->B, 2, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	status = matrix_ops_print(matrix->B);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	printf("=======matrix->C=========\n");
+	status = matrix_ops_set(matrix->C, 0, 0, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->C, 0, 1, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_set(matrix->C, 0, 2, 1);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+	status = matrix_ops_print(matrix->C);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	printf("=======matrix_exp_exponential=========\n");
+	Matrix* result = matrix_core_create_square(3, &status);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	status = matrix_ops_set_zero(result);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
+	status = matrix_exp_exponential(matrix->A, 2, result);
+	if (status != CORE_ERROR_SUCCESS) {
+		CORE_ERROR_PRINT_CALL_AND_LAST(status);
+	}
+
 	printf("hello world!\n");
 }
 
